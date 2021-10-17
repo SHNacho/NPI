f7a768bd41cfa06d6f80dd13c28e54a605a718b2
Nacho Sánchez <nachosanchezh@hotmail.com>
2021-10-17 19:18:31 +0200

    Poca cosa

--- app/src/androidTest/java/com/example/npi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/npi/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.npi", appContext.getPackageName());
+        assertEquals("com.example.NPI", appContext.getPackageName());
     }
 }
