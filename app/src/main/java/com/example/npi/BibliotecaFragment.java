ac58a30ba67713fbc324156547cf6deeb418f14b
jcasado <1eselnumeroXD>
2021-10-17 21:42:27 +0200

    asd

--- app/src/main/java/com/example/npi/BibliotecaFragment.java
+++ app/src/main/java/com/example/npi/BibliotecaFragment.java
@@ -1,28 +1,20 @@
 package com.example.npi;
-
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BibliotecaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.*;
+import androidx.fragment.app.Fragment;
+
 public class BibliotecaFragment extends Fragment {
 
     public BibliotecaFragment(){
         // require a empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_biblioteca, container, false);
     }
--- app/src/main/java/com/example/npi/MainActivity.java
+++ app/src/main/java/com/example/npi/MainActivity.java
@@ -3,56 +3,40 @@ package com.example.npi;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.location.GnssAntennaInfo;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
 import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
 
     private BottomNavigationView bottomNavigationView;
-    private TextView infoTextView;
 
     HomeFragment homeFragment = new HomeFragment();
     BibliotecaFragment bibliotecaFragment = new BibliotecaFragment();
-    HorarioFragment horarioFragment = new HorarioFragment();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoTextView = (TextView) findViewById(R.id.textView);
-
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-
         bottomNavigationView.setOnItemSelectedListener(this);
-        //bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.bibliotecaFragment);
     }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-//            case R.id.home:
-//                getSupportFragmentManager().beginTransaction().add(R.id.container, homeFragment).commit();
-//                infoTextView.setText("Hola");
-//                return true;
-
-            case R.id.biblioteca:
-                //infoTextView.setText("Hola");
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, bibliotecaFragment).commit();
+            case R.id.homeFragment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
                 return true;
-
-            case R.id.horario:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, horarioFragment).commit();
+            case R.id.bibliotecaFragment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, bibliotecaFragment).commit();
                 return true;
         }
         return false;
